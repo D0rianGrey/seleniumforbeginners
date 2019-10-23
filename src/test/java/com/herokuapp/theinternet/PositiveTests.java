f930a4af90ffed5284312267b46a5378454ec0a6
yvake <utyfdfrthby08@gmail.com>
2019-10-23 20:59:04 +0300

    from work

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,9 +1,29 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class PositiveTests {
 
+    @Test
     public void loginTest(){
+        System.out.println("Starting login test");
+        WebDriver driver = new ChromeDriver();
+        sleep(2000);
+        driver.manage().window().maximize();
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened.");
+        driver.close();
+    }
 
+    private void sleep(long m) {
+        try {
+            Thread.sleep(m);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
