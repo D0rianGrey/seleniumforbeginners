5498aecb2ccb90cace330e204bffb010f14091ad
d0riangrey <Q852456357159q>
2019-10-24 00:07:00 +0300

    from mac

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,13 +1,17 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
 
     @Test
-    public void loginTest(){
+    public void loginTest() {
         System.out.println("Starting login test");
         WebDriver driver = new ChromeDriver();
         sleep(2000);
@@ -15,7 +19,32 @@ public class PositiveTests {
         String url = "https://the-internet.herokuapp.com/login";
         driver.get(url);
         System.out.println("Page is opened.");
-        driver.close();
+
+        WebElement userName = driver.findElement(By.xpath("//input[@id='username']"));
+        userName.sendKeys("tomsmith");
+        WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+        password.sendKeys("SuperSecretPassword!");
+        WebElement loginButton = driver.findElement(By.xpath("//button[@class='radius']"));
+        loginButton.click();
+
+        String expectedUrl = "https://the-internet.herokuapp.com/secure";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl,expectedUrl, "Assertion is failed");
+
+
+        WebElement logOut = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
+        Assert.assertTrue(logOut.isDisplayed(),"Logout is not visible");
+
+
+        WebElement succsessfulMessage = driver.findElement(By.xpath("//div[@id='flash']"));
+        String expectedMessage = "You logged into a secure area!";
+        String actualMessage = succsessfulMessage.getText();
+        //Assert.assertEquals(actualMessage,expectedMessage, "Assertion is failed");
+        Assert.assertTrue(actualMessage.contains(expectedMessage),"Assertion is failed\n");
+
+
+
+        driver.quit();
     }
 
     private void sleep(long m) {
